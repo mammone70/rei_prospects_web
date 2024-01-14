0b5b59059e6b00531a5babef4e4d55491b8441a1
Mammone <mammone@gmail.com>
2024-01-13 21:51:05 -0800

    Dynamically generate field mapping inputs based on CSV headers and Prospect Fields

--- src/components/CSVUploadForm.js
+++ src/components/CSVUploadForm.js
@@ -38,7 +38,7 @@ export default function CSVUploadForm({prospectFields}) {
     const delta = currentStep - previousStep
 
     const [file, setFile] = useState();
-    const [csvHeaders, setCsvHeaders] = useState();
+    const [csvHeaders, setCsvHeaders] = useState([]);
     const fileReader = new FileReader();
 
     const {toast} = useContext(ToastContext);
@@ -86,7 +86,7 @@ export default function CSVUploadForm({prospectFields}) {
   }
 
   const handleFileChange = (e) => {
-    console.log(prospectFields);
+    // console.log(prospectFields);
 
     const file = e.target.files[0];
     setFile(file);
@@ -211,83 +211,42 @@ export default function CSVUploadForm({prospectFields}) {
             animate={{ x: 0, opacity: 1 }}
             transition={{ duration: 0.3, ease: 'easeInOut' }}
           >
-            <h2 className='text-base font-semibold leading-7'>
-              Map File Headers
+            <h2 className='text-lg font-semibold leading-7'>
+                Map File Headers
             </h2>
-            <p className='mt-1 text-sm leading-6 text-gray-600'>
-              Map .csv file headers to Prospect fields.
+            <p className='mt-1 text-base leading-6 text-gray-600'>
+                Map .csv file headers to Prospect fields.
             </p>
-
-            <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>          
-              <div className='sm:col-span-2 sm:col-start-1'>
-                <label
-                  htmlFor='city'
-                  className='block text-sm font-medium leading-6 text-gray-900'
-                >
-                  City
-                </label>
-                <div className='mt-2'>
-                  <input
-                    type='text'
-                    id='city'
-                    {...register('city')}
-                    autoComplete='address-level2'
-                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6'
-                  />
-                  {errors.city?.message && (
-                    <p className='mt-2 text-sm text-red-400'>
-                      {errors.city.message}
-                    </p>
-                  )}
-                </div>
-              </div>
-
-              <div className='sm:col-span-2'>
-                <label
-                  htmlFor='state'
-                  className='block text-sm font-medium leading-6 text-gray-900'
-                >
-                  State / Province
-                </label>
-                <div className='mt-2'>
-                  <input
-                    type='text'
-                    id='state'
-                    {...register('state')}
-                    autoComplete='address-level1'
-                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6'
-                  />
-                  {errors.state?.message && (
-                    <p className='mt-2 text-sm text-red-400'>
-                      {errors.state.message}
-                    </p>
-                  )}
-                </div>
-              </div>
-
-              <div className='sm:col-span-2'>
-                <label
-                  htmlFor='zip'
-                  className='block text-sm font-medium leading-6 text-gray-900'
-                >
-                  ZIP / Postal code
-                </label>
-                <div className='mt-2'>
-                  <input
-                    type='text'
-                    id='zip'
-                    {...register('zip')}
-                    autoComplete='postal-code'
-                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6'
-                  />
-                  {errors.zip?.message && (
-                    <p className='mt-2 text-sm text-red-400'>
-                      {errors.zip.message}
-                    </p>
-                  )}
-                </div>
-              </div>
-            </div>
+            
+                {csvHeaders.map((header, index) => (
+                    <>
+                        <div class="md:flex md:items-center mb-6 mt-6">
+                            <div class="md:w-1/2">
+                                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for={index}>
+                                    {header}
+                                </label>
+                            </div>
+                            <div class="md:w-1/2">
+                                <select class="block appearance-none w-48 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id={index}>
+                                    <option value=""></option>
+                                    {prospectFields.map((prospectField, index) => (
+                                        <>
+                                            <option value={prospectField.fieldName}>
+                                                {
+                                                    prospectField.required ? (
+                                                        `${prospectField.displayName}*`
+                                                    ) :(
+                                                        prospectField.displayName
+                                                    )
+                                                }
+                                            </option>
+                                        </>     
+                                    ))}
+                                </select>    
+                            </div>
+                        </div>
+                    </>
+                ))}
           </motion.div>
         )}
 
